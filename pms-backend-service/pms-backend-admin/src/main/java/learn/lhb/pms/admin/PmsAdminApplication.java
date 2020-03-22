d7ef1aec941bdb16334591e0f5edda99acc28e26
lhb <782272323@qq.com>
2020-03-23 00:14:40 +0800

    后台服务基本环境搭建完成

---
+++ pms-backend-service/pms-backend-admin/src/main/java/learn/lhb/pms/admin/PmsAdminApplication.java
@@ -0,0 +1,16 @@
+package learn.lhb.pms.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 梁鸿斌
+ * @date 2020/3/23.
+ * @time 00:04
+ */
+@SpringBootApplication
+public class PmsAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PmsAdminApplication.class, args);
+    }
+}
